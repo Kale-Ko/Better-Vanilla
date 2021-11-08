6e2682b8053d9e2f235608d105e95a06ce183c7b
Kale-Ko <github@kaleko.ga>
2021-11-08 15:45:59 -0500

    Update

--- src/main/java/com/kale_ko/better_vanilla/config/Config.java
+++ src/main/java/com/kale_ko/better_vanilla/config/Config.java
@@ -1,5 +1,38 @@
 package com.kale_ko.better_vanilla.config;
 
-public class Config {
+import me.shedaniel.clothconfig2.api.ConfigBuilder;
+import me.shedaniel.clothconfig2.api.ConfigCategory;
+import net.minecraft.text.TranslatableText;
 
+public class Config {
+    public boolean no_modded_info = true;
+
+    public boolean creepers_ignite_from_fire = true;
+    public boolean creepers_defuse_in_water = true;
+
+    public ConfigBuilder buildMenu() {
+        ConfigBuilder builder = ConfigBuilder.create().setTitle(new TranslatableText("better_vanilla.config.title"));
+
+        ConfigCategory general = builder.getOrCreateCategory(new TranslatableText("better_vanilla.config.general"));
+
+        general.addEntry(builder.entryBuilder()
+                .startBooleanToggle(new TranslatableText("better_vanilla.config.no_modded_info.title"), no_modded_info)
+                .setDefaultValue(true)
+                .setTooltip(new TranslatableText("better_vanilla.config.no_modded_info.description"))
+                .build());
+
+        general.addEntry(builder.entryBuilder()
+                .startBooleanToggle(new TranslatableText("better_vanilla.config.creepers_ignite_from_fire.title"), creepers_ignite_from_fire)
+                .setDefaultValue(true)
+                .setTooltip(new TranslatableText("better_vanilla.config.creepers_ignite_from_fire.description"))
+                .build());
+
+        general.addEntry(builder.entryBuilder()
+                .startBooleanToggle(new TranslatableText("better_vanilla.config.creepers_defuse_in_water.title"), creepers_defuse_in_water)
+                .setDefaultValue(true)
+                .setTooltip(new TranslatableText("better_vanilla.config.creepers_defuse_in_water.description"))
+                .build());
+
+        return builder;
+    }
 }
--- src/main/java/com/kale_ko/better_vanilla/config/ConfigMenu.java
+++ src/main/java/com/kale_ko/better_vanilla/config/ConfigMenu.java
@@ -1,5 +1,14 @@
 package com.kale_ko.better_vanilla.config;
 
-public class ConfigMenu {
+import com.kale_ko.better_vanilla.Main;
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
 
+public class ConfigMenu implements ModMenuApi {
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return parent -> {
+            return Main.config.buildMenu().setParentScreen(parent).build();
+        };
+    }
 }
