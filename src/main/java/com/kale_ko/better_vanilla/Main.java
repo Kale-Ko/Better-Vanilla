26e1054e80cdac0b2efb58e4a38838f3de6a050f
Kale-Ko <github@kaleko.dev>
2021-11-08 15:45:59 -0500

    Update

--- src/main/java/com/kale_ko/better_vanilla/Main.java
+++ src/main/java/com/kale_ko/better_vanilla/Main.java
@@ -3,9 +3,11 @@ package com.kale_ko.better_vanilla;
 import net.fabricmc.api.ModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import com.kale_ko.better_vanilla.config.Config;
 
 public class Main implements ModInitializer {
     public static final Logger Console = LogManager.getLogger("better-vanilla");
+    public static final Config config = new Config();
 
     @Override
     public void onInitialize() {
