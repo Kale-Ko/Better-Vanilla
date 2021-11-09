e47e7521d6a8745d360afabf45422d60c70ef5ed
Kale-Ko <github@kaleko.ga>
2021-11-08 20:21:21 -0500

    Update

---
+++ src/main/java/com/kale_ko/better_vanilla/mixins/RemoveModdedNotice.java
@@ -0,0 +1,16 @@
+package com.kale_ko.better_vanilla.mixins;
+
+import com.kale_ko.better_vanilla.Main;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import net.minecraft.client.MinecraftClient;
+
+@Mixin(MinecraftClient.class)
+public class RemoveModdedNotice {
+    @Inject(at = @At("HEAD"), method = "isModded()Z", cancellable = true)
+    public void isModded(CallbackInfoReturnable<Boolean> info) {
+        info.setReturnValue(!Main.config.remove_modded_notice);
+    }
+}
