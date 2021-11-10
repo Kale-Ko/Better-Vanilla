ad3f9d054702bec854f99e11d83aad83ca30d111
Kale-Ko <github@kaleko.ga>
2021-11-09 20:02:04 -0500

    Update

--- src/main/java/com/kale_ko/better_vanilla/BookshelfBlock.java
+++ src/main/java/com/kale_ko/better_vanilla/BookshelfBlock.java
@@ -1,4 +1,4 @@
-U/**
+/**
     @license
     MIT License
     Copyright (c) 2021 Kale Ko
@@ -8,11 +8,58 @@ U/**
 package com.kale_ko.better_vanilla;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Items;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.IntProperty;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class BookshelfBlock extends Block {
+    public static final IntProperty BOOKS;
+
     public BookshelfBlock(Settings settings) {
         super(settings);
+        this.setDefaultState(this.stateManager.getDefaultState().with(BOOKS, 3));
+    }
 
-        Main.Console.info("Created bookshelf!");
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        if (!Main.config.bookshelves_hold_books) return ActionResult.FAIL;
+
+        if (state.get(BOOKS) > 0 && player.getInventory().getMainHandStack().isEmpty()) {
+            if (player.getInventory().getOccupiedSlotWithRoomForStack(Items.BOOK.getDefaultStack()) == -1) {
+                player.getInventory().setStack(player.getInventory().getEmptySlot(), Items.BOOK.getDefaultStack());
+            } else {
+                player.getInventory().getStack(player.getInventory().getOccupiedSlotWithRoomForStack(Items.BOOK.getDefaultStack())).setCount(player.getInventory().getStack(player.getInventory().getOccupiedSlotWithRoomForStack(Items.BOOK.getDefaultStack())).getCount() + 1);
+            }
+
+            state = state.with(BOOKS, state.get(BOOKS) - 1);
+            world.setBlockState(pos, state, Block.NOTIFY_ALL);
+
+            return ActionResult.SUCCESS;
+        } else if (state.get(BOOKS) < 3 && player.getInventory().getMainHandStack().getItem() == Items.BOOK) {
+            player.getInventory().getStack(player.getInventory().getOccupiedSlotWithRoomForStack(Items.BOOK.getDefaultStack())).setCount(player.getInventory().getStack(player.getInventory().getOccupiedSlotWithRoomForStack(Items.BOOK.getDefaultStack())).getCount() - 1);
+
+            state = state.with(BOOKS, state.get(BOOKS) + 1);
+            world.setBlockState(pos, state, Block.NOTIFY_ALL);
+
+            return ActionResult.SUCCESS;
+        } else {
+            return ActionResult.FAIL;
+        }
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(BOOKS);
+    }
+
+    static {
+        BOOKS = IntProperty.of("books", 0, 3);
     }
 }
