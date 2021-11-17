3c81a5f999618f7e20d0cb72c2c5ebc2f2c0c629
Kale-Ko <github@kaleko.ga>
2021-11-16 19:08:23 -0500

    Update

---
+++ src/main/java/com/kale_ko/better_vanilla/CustomDamageSource.java
@@ -0,0 +1,9 @@
+package com.kale_ko.better_vanilla;
+
+import net.minecraft.entity.damage.DamageSource;
+
+public class CustomDamageSource extends DamageSource {
+    public CustomDamageSource(String name) {
+        super(name);
+    }
+}
