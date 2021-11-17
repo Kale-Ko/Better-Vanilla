370474e571044dd7ab2165db6b35af62c44606f8
Kale-Ko <github@kaleko.ga>
2021-11-17 13:51:17 -0500

    Update

--- src/main/java/com/kale_ko/better_vanilla/config/Config.java
+++ src/main/java/com/kale_ko/better_vanilla/config/Config.java
@@ -12,40 +12,47 @@ import net.fabricmc.loader.api.FabricLoader;
 import me.shedaniel.clothconfig2.api.ConfigBuilder;
 import me.shedaniel.clothconfig2.api.ConfigCategory;
 import me.shedaniel.clothconfig2.gui.entries.BooleanListEntry;
+import me.shedaniel.clothconfig2.gui.entries.FloatListEntry;
+import me.shedaniel.clothconfig2.gui.entries.StringListEntry;
 import net.minecraft.text.TranslatableText;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Config {
     public static final String version = "2";
 
     public static final File configFile = FabricLoader.getInstance().getConfigDir().resolve("better_vanilla.config").toFile();
 
-    public void create(ConfigKey config) {
+    private Map<ConfigKey, Object> configRegistry = new HashMap<ConfigKey, Object>();
 
+    public void create(ConfigKey config) {
+        configRegistry.put(config, config.defaultValue);
     }
 
     public Object get(String id) {
+        for (Map.Entry<ConfigKey, Object> kvp : configRegistry.entrySet()) {
+            if (kvp.getKey().id.equalsIgnoreCase(id)) {
+                return kvp.getValue();
+            }
+        }
 
+        return null;
     }
 
     public void save() {
-        String data = "version=" + version + "\n";
+        StringBuilder data = new StringBuilder("version=" + version + "\n");
 
-        data += "remove_modded_notice=" + remove_modded_notice + "\n";
-        data += "creepers_ignite_from_fire=" + creepers_ignite_from_fire + "\n";
-        data += "creepers_defuse_in_water=" + creepers_defuse_in_water + "\n";
-        data += "infinity_plus_mending=" + infinity_plus_mending + "\n";
-        data += "bookshelves_hold_books=" + bookshelves_hold_books + "\n";
-        data += "stonecutters_deal_damage=" + stonecutters_deal_damage + "\n";
-        data += "campfires_set_fire=" + campfires_set_fire + "\n";
-        data += "disable_custom_blocks=" + disable_custom_blocks + "\n";
+        for (Map.Entry<ConfigKey, Object> kvp : configRegistry.entrySet()) {
+            data.append(kvp.getKey().id + "=" + kvp.getValue().toString() + "\n");
+        }
 
         try {
             FileWriter configWriter = new FileWriter(configFile);
-            configWriter.write(data);
+            configWriter.write(data.toString());
             configWriter.close();
         } catch (IOException err) {
             Main.Console.error("Failed to save config");
@@ -81,22 +88,11 @@ public class Config {
             for (String line : lines) {
                 String[] keyValue = line.split("=");
 
-                if (keyValue[0].equals("remove_modded_notice")) {
-                    remove_modded_notice = Boolean.parseBoolean(keyValue[1]);
-                } else if (keyValue[0].equals("creepers_ignite_from_fire")) {
-                    creepers_ignite_from_fire = Boolean.parseBoolean(keyValue[1]);
-                } else if (keyValue[0].equals("creepers_defuse_in_water")) {
-                    creepers_defuse_in_water = Boolean.parseBoolean(keyValue[1]);
-                } else if (keyValue[0].equals("infinity_plus_mending")) {
-                    infinity_plus_mending = Boolean.parseBoolean(keyValue[1]);
-                } else if (keyValue[0].equals("bookshelves_hold_books")) {
-                    bookshelves_hold_books = Boolean.parseBoolean(keyValue[1]);
-                } else if (keyValue[0].equals("stonecutters_deal_damage")) {
-                    stonecutters_deal_damage = Boolean.parseBoolean(keyValue[1]);
-                } else if (keyValue[0].equals("campfires_set_fire")) {
-                    campfires_set_fire = Boolean.parseBoolean(keyValue[1]);
-                } else if (keyValue[0].equals("disable_custom_blocks")) {
-                    disable_custom_blocks = Boolean.parseBoolean(keyValue[1]);
+                for (Map.Entry<ConfigKey, Object> kvp : configRegistry.entrySet()) {
+                    if (keyValue[0].equalsIgnoreCase(kvp.getKey().id)) {
+                        configRegistry.remove(kvp.getKey());
+                        configRegistry.put(kvp.getKey(), keyValue[1]);
+                    }
                 }
             }
         } catch (Exception err) {
@@ -110,98 +106,51 @@ public class Config {
         ConfigCategory general = builder.getOrCreateCategory(new TranslatableText("better_vanilla.config.category.general"));
         ConfigCategory advanced = builder.getOrCreateCategory(new TranslatableText("better_vanilla.config.category.advanced"));
 
-        BooleanListEntry remove_modded_notice_entry = builder.entryBuilder()
-                .startBooleanToggle(new TranslatableText("better_vanilla.config.option.remove_modded_notice.title"), remove_modded_notice)
-                .setDefaultValue(true)
-                .setTooltip(new TranslatableText("better_vanilla.config.option.remove_modded_notice.description"))
-                .setYesNoTextSupplier((Boolean value) -> {
-                    return new TranslatableText("better_vanilla.config.value." + value.toString());
-                })
-                .build();
+        for (Map.Entry<ConfigKey, Object> kvp : configRegistry.entrySet()) {
+            if (kvp.getKey().type == ConfigType.String) {
+                StringListEntry entry = builder.entryBuilder()
+                        .startStrField(new TranslatableText("better_vanilla.config.option." + kvp.getKey().id + ".title"), (String) kvp.getValue())
+                        .setDefaultValue((String) kvp.getKey().defaultValue)
+                        .setTooltip(new TranslatableText("better_vanilla.config.option." + kvp.getKey().id + ".description"))
+                        .build();
 
-        BooleanListEntry creepers_ignite_from_fire_entry = builder.entryBuilder()
-                .startBooleanToggle(new TranslatableText("better_vanilla.config.option.creepers_ignite_from_fire.title"), creepers_ignite_from_fire)
-                .setDefaultValue(true)
-                .setTooltip(new TranslatableText("better_vanilla.config.option.creepers_ignite_from_fire.description"))
-                .setYesNoTextSupplier((Boolean value) -> {
-                    return new TranslatableText("better_vanilla.config.value." + value.toString());
-                })
-                .build();
+                if (kvp.getKey().category == com.kale_ko.better_vanilla.config.ConfigCategory.General) {
+                    general.addEntry(entry);
+                } else if (kvp.getKey().category == com.kale_ko.better_vanilla.config.ConfigCategory.Advanced) {
+                    advanced.addEntry(entry);
+                }
+            } else if (kvp.getKey().type == ConfigType.Number) {
+                FloatListEntry entry = builder.entryBuilder()
+                        .startFloatField(new TranslatableText("better_vanilla.config.option." + kvp.getKey().id + ".title"), (Float) kvp.getValue())
+                        .setDefaultValue((Float) kvp.getKey().defaultValue)
+                        .setTooltip(new TranslatableText("better_vanilla.config.option." + kvp.getKey().id + ".description"))
+                        .build();
 
-        BooleanListEntry creepers_defuse_in_water_entry = builder.entryBuilder()
-                .startBooleanToggle(new TranslatableText("better_vanilla.config.option.creepers_defuse_in_water.title"), creepers_defuse_in_water)
-                .setDefaultValue(true)
-                .setTooltip(new TranslatableText("better_vanilla.config.option.creepers_defuse_in_water.description"))
-                .setYesNoTextSupplier((Boolean value) -> {
-                    return new TranslatableText("better_vanilla.config.value." + value.toString());
-                })
-                .build();
+                if (kvp.getKey().category == com.kale_ko.better_vanilla.config.ConfigCategory.General) {
+                    general.addEntry(entry);
+                } else if (kvp.getKey().category == com.kale_ko.better_vanilla.config.ConfigCategory.Advanced) {
+                    advanced.addEntry(entry);
+                }
+            } else if (kvp.getKey().type == ConfigType.Boolean) {
+                BooleanListEntry entry = builder.entryBuilder()
+                        .startBooleanToggle(new TranslatableText("better_vanilla.config.option." + kvp.getKey().id + ".title"), (Boolean) kvp.getValue())
+                        .setDefaultValue((Boolean) kvp.getKey().defaultValue)
+                        .setTooltip(new TranslatableText("better_vanilla.config.option." + kvp.getKey().id + ".description"))
+                        .setYesNoTextSupplier((Boolean value) -> {
+                            return new TranslatableText("better_vanilla.config.value." + value.toString());
+                        })
+                        .build();
 
-        BooleanListEntry infinity_plus_mending_entry = builder.entryBuilder()
-                .startBooleanToggle(new TranslatableText("better_vanilla.config.option.infinity_plus_mending.title"), infinity_plus_mending)
-                .setDefaultValue(true)
-                .setTooltip(new TranslatableText("better_vanilla.config.option.infinity_plus_mending.description"))
-                .setYesNoTextSupplier((Boolean value) -> {
-                    return new TranslatableText("better_vanilla.config.value." + value.toString());
-                })
-                .build();
-
-        BooleanListEntry bookshelves_hold_books_entry = builder.entryBuilder()
-                .startBooleanToggle(new TranslatableText("better_vanilla.config.option.bookshelves_hold_books.title"), bookshelves_hold_books)
-                .setDefaultValue(true)
-                .setTooltip(new TranslatableText("better_vanilla.config.option.bookshelves_hold_books.description"))
-                .setYesNoTextSupplier((Boolean value) -> {
-                    return new TranslatableText("better_vanilla.config.value." + value.toString());
-                })
-                .build();
-
-        BooleanListEntry stonecutters_deal_damage_entry = builder.entryBuilder()
-                .startBooleanToggle(new TranslatableText("better_vanilla.config.option.stonecutters_deal_damage.title"), stonecutters_deal_damage)
-                .setDefaultValue(false)
-                .setTooltip(new TranslatableText("better_vanilla.config.option.stonecutters_deal_damage.description"))
-                .requireRestart().setYesNoTextSupplier((Boolean value) -> {
-                    return new TranslatableText("better_vanilla.config.value." + value.toString());
-                })
-                .build();
-
-        BooleanListEntry campfires_set_fire_entry = builder.entryBuilder()
-                .startBooleanToggle(new TranslatableText("better_vanilla.config.option.campfires_set_fire.title"), campfires_set_fire)
-                .setDefaultValue(false)
-                .setTooltip(new TranslatableText("better_vanilla.config.option.campfires_set_fire.description"))
-                .requireRestart().setYesNoTextSupplier((Boolean value) -> {
-                    return new TranslatableText("better_vanilla.config.value." + value.toString());
-                })
-                .build();
-
-        BooleanListEntry disable_custom_blocks_entry = builder.entryBuilder()
-                .startBooleanToggle(new TranslatableText("better_vanilla.config.option.disable_custom_blocks.title"), disable_custom_blocks)
-                .setDefaultValue(false)
-                .setTooltip(new TranslatableText("better_vanilla.config.option.disable_custom_blocks.description"))
-                .requireRestart().setYesNoTextSupplier((Boolean value) -> {
-                    return new TranslatableText("better_vanilla.config.value." + value.toString());
-                })
-                .build();
-
-        general.addEntry(remove_modded_notice_entry);
-        general.addEntry(creepers_ignite_from_fire_entry);
-        general.addEntry(creepers_defuse_in_water_entry);
-        general.addEntry(infinity_plus_mending_entry);
-        general.addEntry(bookshelves_hold_books_entry);
-        general.addEntry(stonecutters_deal_damage_entry);
-        general.addEntry(campfires_set_fire_entry);
-        advanced.addEntry(disable_custom_blocks_entry);
+                if (kvp.getKey().category == com.kale_ko.better_vanilla.config.ConfigCategory.General) {
+                    general.addEntry(entry);
+                } else if (kvp.getKey().category == com.kale_ko.better_vanilla.config.ConfigCategory.Advanced) {
+                    advanced.addEntry(entry);
+                }
+            }
+        }
 
         builder.setFallbackCategory(general);
         builder.setSavingRunnable(() -> {
-            remove_modded_notice = remove_modded_notice_entry.getValue();
-            creepers_ignite_from_fire = creepers_ignite_from_fire_entry.getValue();
-            creepers_defuse_in_water = creepers_defuse_in_water_entry.getValue();
-            infinity_plus_mending = infinity_plus_mending_entry.getValue();
-            bookshelves_hold_books = bookshelves_hold_books_entry.getValue();
-            stonecutters_deal_damage = stonecutters_deal_damage_entry.getValue();
-            campfires_set_fire = campfires_set_fire_entry.getValue();
-            disable_custom_blocks = disable_custom_blocks_entry.getValue();
-
             save();
         });
 
