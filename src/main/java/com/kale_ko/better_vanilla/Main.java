4bbfaa43400665b94d4a22c39f1fa61ddd445fe7
Kale-Ko <github@kaleko.dev>
2021-11-16 20:07:18 -0500

    Update

--- src/main/java/com/kale_ko/better_vanilla/Main.java
+++ src/main/java/com/kale_ko/better_vanilla/Main.java
@@ -10,19 +10,30 @@ package com.kale_ko.better_vanilla;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.MapColor;
 import net.minecraft.block.Material;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.registry.Registry;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import java.util.function.ToIntFunction;
+
 import com.kale_ko.better_vanilla.config.Config;
 
 public class Main implements ModInitializer {
     public static final Logger Console = LogManager.getLogger("better_vanilla");
     public static final Config config = new Config();
+
+    private static ToIntFunction<BlockState> createLightLevelFromLitBlockState(int litLevel) {
+        return state -> {
+            return (Boolean) state.get(Properties.LIT) ? litLevel : 0;
+        };
+    }
 
     @Override
     public void onInitialize() {
@@ -33,20 +44,17 @@ public class Main implements ModInitializer {
         if (!config.disable_custom_blocks) {
             Block bookshelf = new BookshelfBlock(AbstractBlock.Settings.of(Material.WOOD).strength(1.5F).sounds(BlockSoundGroup.WOOD));
             Registry.register(Registry.BLOCK, 144, "bookshelf", bookshelf);
-
             BlockItem bookshelf_item = new BlockItem(bookshelf, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS));
             Registry.register(Registry.ITEM, 233, "bookshelf", bookshelf_item);
 
-            Block stonecutter = new DamagingStonecutterBlock(AbstractBlock.Settings.of(Material.WOOD).strength(1.5F).sounds(BlockSoundGroup.WOOD));
+            Block stonecutter = new DamagingStonecutterBlock(AbstractBlock.Settings.of(Material.STONE).requiresTool().strength(3.5F));
             Registry.register(Registry.BLOCK, 690, "stonecutter", stonecutter);
-
-            BlockItem stonecutter_item = new BlockItem(stonecutter, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS));
+            BlockItem stonecutter_item = new BlockItem(stonecutter, new Item.Settings().group(ItemGroup.DECORATIONS));
             Registry.register(Registry.ITEM, 1049, "stonecutter", stonecutter_item);
-            
-            Block campfire = new BurringCampFireBlock(AbstractBlock.Settings.of(Material.WOOD).strength(1.5F).sounds(BlockSoundGroup.WOOD));
-            Registry.register(Registry.BLOCK, 694, "campfire", campfire);
 
-            BlockItem campfire_item = new BlockItem(campfire, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS));
+            Block campfire = new BurringCampFireBlock(AbstractBlock.Settings.of(Material.WOOD, MapColor.SPRUCE_BROWN).strength(2.0F).sounds(BlockSoundGroup.WOOD).luminance(createLightLevelFromLitBlockState(15)).nonOpaque());
+            Registry.register(Registry.BLOCK, 694, "campfire", campfire);
+            BlockItem campfire_item = new BlockItem(campfire, new Item.Settings().group(ItemGroup.DECORATIONS));
             Registry.register(Registry.ITEM, 1055, "campfire", campfire_item);
         }
 
