4f6a6879c26211c92b727e95c314860d1f7ec9e7
Kale-Ko <github@kaleko.dev>
2021-11-29 18:49:41 -0500

    Update

---
+++ src/main/java/com/kale_ko/better_vanilla/mixins/GameRendererAccessor.java
@@ -0,0 +1,21 @@
+package com.kale_ko.better_vanilla.mixins;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.GameRenderer;
+
+@Mixin(GameRenderer.class)
+public interface GameRendererAccessor {
+    @Accessor
+    Boolean getRenderingPanorama();
+
+    @Accessor
+    MinecraftClient getClient();
+
+    @Accessor
+    double getMovementFovMultiplier();
+
+    @Accessor
+    double getLastMovementFovMultiplier();
+}
