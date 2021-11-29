c37da3f38fc07ae2309e0394ca11178538c5fd1f
Kale-Ko <github@kaleko.dev>
2021-11-29 16:26:42 -0500

    Update

--- src/main/java/com/kale_ko/better_vanilla/mixins/Zoom.java
+++ src/main/java/com/kale_ko/better_vanilla/mixins/Zoom.java
@@ -12,28 +12,27 @@ import net.minecraft.util.math.Vec3d;
 
 @Mixin(Camera.class)
 public class Zoom {
-    public Boolean zoomed = false;
+    public static Boolean zoomed = false;
 
     @Inject(at = @At("HEAD"), method = "getProjection()Lnet/minecraft/client/render/Camera$Projection;", cancellable = true)
     public void getProjection(CallbackInfoReturnable<Camera.Projection> info) throws Exception {
         MinecraftClient minecraftClient = MinecraftClient.getInstance();
         double d = (double) minecraftClient.getWindow().getFramebufferWidth() / (double) minecraftClient.getWindow().getFramebufferHeight();
-        double e = Math.tan(getFov() * 0.01745329238474369 / 2.0) * (double) 0.05f;
+        double e = Math.tan((minecraftClient.options.fov / getZoom()) * 0.01745329238474369 / 2.0) * (double) 0.05f;
         double f = e * d;
         Vec3d vec3d = new Vec3d(((CameraAccessor) ((Camera) ((Object) this))).getHorizontalPlane()).multiply(0.05f);
         Vec3d vec3d2 = new Vec3d(((CameraAccessor) ((Camera) ((Object) this))).getDiagonalPlane()).multiply(f);
         Vec3d vec3d3 = new Vec3d(((CameraAccessor) ((Camera) ((Object) this))).getVerticalPlane()).multiply(e);
 
-        info.setReturnValue((Camera.Projection) Camera.Projection.class.getConstructors()[0].newInstance(vec3d, vec3d2, vec3d3));
+        info.setReturnValue(
+                (Camera.Projection) Camera.Projection.class.getConstructors()[0].newInstance(vec3d, vec3d2, vec3d3));
     }
 
-    public double getFov() {
-        MinecraftClient minecraftClient = MinecraftClient.getInstance();
-
-        if (zoomed) {
-            return 30f;
+    public double getZoom() {
+        if ((Boolean) Main.config.get("zoom_enabled", ConfigType.Boolean) && zoomed) {
+            return (double) Main.config.get("zoom_amount", ConfigType.Number);
         } else {
-            return minecraftClient.options.fov;
+            return 1d;
         }
     }
 }
